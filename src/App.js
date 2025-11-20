04dfdea6f0f4d83c734bdde93436dc3c7adce778
KimHancheol <gimhancheol4@gmail.com>
2025-11-20 10:06:34 +0900

    안뇽

--- src/App.js
+++ src/App.js
@@ -4,20 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      아룡하세요~~ 효자가수 한숙이입니다~!!
     </div>
   );
 }
